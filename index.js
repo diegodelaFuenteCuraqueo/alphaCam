e576731ed01803793f55a3dfe2141b4c39a6a7ed
delaefe <ddelaefe@gmail.com>
2023-03-04 16:40:52 -0300

    image and webcam

--- index.js
+++ index.js
@@ -18,16 +18,19 @@ const webcam = (p) => {
 }
 
 const image = (p) => {
+  let img
   p.setup = () => {
     var canvas = p.createCanvas(800, 600)
     canvas.parent('#image')
+    img = p.loadImage('https://3.bp.blogspot.com/-gMq_vCCFvw0/UbYyiWjZi3I/AAAAAAAAAIY/Ni_pXBOxv7o/s1600/Butterfly+Pictures-01.jpg')
   }
 
   p.draw = () => {
-    p.background(0, 0, 51)
-    p.fill(0, 255, 0)
+    //p.background(0, 0, 51)
+    p.fill(0, 255, 0, 95)
     p.rectMode(p.CENTER)
-    p.rect(p.width / 2, p.height / 2, 50, 50)
+    p.rect(p.width / 2, p.height / 2, 100, 100)
+    p.image(img, 0, 0, p.width, p.height)
   }
 }
 
